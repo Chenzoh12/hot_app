a709a746bbf89de00e2e83652c10c2661ff6f34f
Vincent Scotti <scottivincentj@gmail.com>
2018-02-04 22:44:42 +0000

    Current able to add to firebase with unique key and name.

--- hot-app/src/components/Inventory.js
+++ hot-app/src/components/Inventory.js
@@ -4,43 +4,51 @@ import fire from './fire';
 class Inventory extends Component { 
     constructor(props) {
         super(props);
-        this.state = { 
-            cigars: []   
-            
+        this.state = {
+            cigars: []
         };
+        
+        this.addCigar = this.addCigar.bind(this);
     }
     
     componentWillMount(){
-        /* Create reference to cigars in Firebase Database */
-        let cigarsRef = fire.database().ref('cigars').orderByKey().limitToLast(350);
-        cigarsRef.on('child_added', snapshot => {
+        // Create reference to cigar db
+        let cigarDB = fire.database().ref('cigars').orderByKey();
+        
+        cigarDB.on('child_added', snapshot => {
             /* Update React state when cigar is added at Firebase Database */
             let cigar = { text: snapshot.val(), id: snapshot.key };
             this.setState({ cigars: [cigar].concat(this.state.cigars) });
         })
+        
     }
     
     addCigar(e){
-        e.preventDefault(); // <- prevent form submit from reloading the page
-        /* Send the message to Firebase */
-        fire.database().ref('cigars').push( this.inputEl.value );
-        this.inputEl.value = ''; // <- clear the input
+        
+        //Prevent page refresh when new product added
+        e.preventDefault();
+        console.log("Here:" + this.name.value);
+        
+        fire.database().ref('cigars').push( this.name.value );
     }
     
     render() {
         return ( 
             <section>
                 <form onSubmit={this.addCigar.bind(this)}>
-                <input type="text" ref={ el => this.inputEl = el }/>
-                <input type="submit"/>
-                    <ul>
-                    { /* Render the list of cigars */
-                        this.state.cigars.map( cigar => <li key={cigar.id}>{cigar.text}</li> )
-                    }
-                </ul>
-                </form>
                 
+                   <label>Cigar Name:</label> 
+                   <input type='text' ref={newName => this.name = newName}/>
+                   <input type='submit'/>
+                   
+                </form>
 
+                <ol>
+                    {
+                        this.state.cigars.map( cigar => <li key={cigar.id}>{cigar.text}</li>)    
+                        
+                    }
+                </ol>
             </section>
         );
     }
